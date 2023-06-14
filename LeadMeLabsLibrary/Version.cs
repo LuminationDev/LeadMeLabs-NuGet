9e37b70de09005350165e4af63083416fa91be71
Edward Coad <ecoad@lumination.com.au>
2023-06-14 09:45:19 +0930

    Version null checks, version bump

--- LeadMeLabsLibrary/Version.cs
+++ LeadMeLabsLibrary/Version.cs
@@ -7,14 +7,21 @@ namespace LeadMeLabsLibrary
     public static class Version
     {
         /// <summary>
-        /// Print out the currently running software version to a text file.
+        /// Print out the currently running software version to a text file at 'programLocation\_logs\version.txt'. 
+        /// If the version number or program directory cannot be found the function returns false, bailing out before
+        /// writing the version. 
         /// </summary>
         public static bool GenerateVersion()
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
             FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string version = fileVersionInfo.ProductVersion;
-            string programLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string? version = fileVersionInfo.ProductVersion;
+            string? programLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            if(version == null || programLocation == null)
+            {
+                return false;
+            }
 
             File.WriteAllText($"{programLocation}\\_logs\\version.txt", version);
 
