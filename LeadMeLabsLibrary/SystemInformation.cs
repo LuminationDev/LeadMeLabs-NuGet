e8ea7c689bd1970080c66523c65f713d406426dd
Edward Coad <ecoad@lumination.com.au>
2023-06-28 13:43:10 +0930

    Create outbound rules and dismiss virtual box mac addresses

--- LeadMeLabsLibrary/SystemInformation.cs
+++ LeadMeLabsLibrary/SystemInformation.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Management;
 using System.Net;
 using System.Net.Sockets;
-using System.Reflection;
-using SysVersion = System.Version;
 
 namespace LeadMeLabsLibrary
 {
@@ -40,64 +37,10 @@ namespace LeadMeLabsLibrary
         /// <returns>A string of the mac address</returns>
         public static string? GetMACAddress()
         {
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapterConfiguration where IPEnabled=true");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = true AND NOT Description LIKE '%VirtualBox%'");
             IEnumerable<ManagementObject> objects = searcher.Get().Cast<ManagementObject>();
-            string? mac = (from o in objects orderby o["IPConnectionMetric"] select o["MACAddress"].ToString()).FirstOrDefault();
+            string mac = (from o in objects orderby o["IPConnectionMetric"] select o["MACAddress"].ToString()).FirstOrDefault()?.Replace(":", "-") ?? "Unknown";
             return mac;
-        }
-
-        /// <summary>
-        /// Query the program to get the current version number of the software that is running.
-        /// </summary>
-        /// <returns>A string of the version number in the format X.X.X.X</returns>
-        public static string? GetVersionNumber()
-        {
-            Assembly? assembly = Assembly.GetExecutingAssembly();
-            if (assembly == null) return "N/A";
-
-            SysVersion? version = assembly.GetName().Version;
-            if (version == null) return "N/A";
-
-            // Format the version number as Major.Minor.Build
-            return $"{version.Major}.{version.Minor}.{version.Build}";
-        }
-
-        /// <summary>
-        /// Print out the currently running software version to a text file at 'programLocation\_logs\version.txt'. 
-        /// If the version number or program directory cannot be found the function returns false, bailing out before
-        /// writing the version. 
-        /// </summary>
-        public static bool GenerateVersion()
-        {
-            string? programLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            if (programLocation == null)
-            {
-                return false;
-            }
-
-            Assembly? assembly = Assembly.GetExecutingAssembly();
-            if (assembly == null)
-            {
-                WriteFile(programLocation, "0.0.0");
-                return false;
-            }
-
-            SysVersion? version = assembly.GetName().Version;
-            if (version == null)
-            {
-                WriteFile(programLocation, "0.0.0");
-                return false;
-            }
-
-            string? formattedVersion = $"{version.Major}.{version.Minor}.{version.Build}";
-            WriteFile(programLocation, formattedVersion);
-
-            return version != null;
-        }
-
-        private static void WriteFile(string location, string version)
-        {
-            File.WriteAllText($"{location}\\_logs\\version.txt", version);
         }
     }
 }
